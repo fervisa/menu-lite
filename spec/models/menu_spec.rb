01bcb62c9f96eeee58f7eb56a698fab2de5d6186
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-02-27 19:31:11 -0600

    Adding validations to Menu

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
   it { should have_and_belong_to_many :platillos }
+  it { should validate_presence_of :nombre }
+  it { should validate_presence_of :fecha }
 end
